de9ff7bd022076c549b635d3104d3dc6ab5b1fd3
jhordy <jhordymundung10@gmail.com>
2020-06-18 16:25:19 +0800

    Pertemuan 11

--- pertemuan11/Classes/src/com/multiplatform/Car.java
+++ pertemuan11/Classes/src/com/multiplatform/Car.java
@@ -1,21 +1,25 @@
 package com.multiplatform;
 
 public class Car {
-    //2 member dalam class
-    //1. field
-    //2. method
+    // 2 member dalam class
+    //1. Field
+    //2. Method
+
     private String model;
     private int doors;
     private int wheels;
     private String engine;
 
-    //setter
+    //Setter
     public void setModel(String model){
         String validModel = model.toLowerCase();
-        if(validModel.equals("agya")||validModel.equals("xenia")) this.model = model;
-        else this.model = "Unknown";
+        if(validModel.equals("agya") || validModel.equals("xenia")){
+            this.model = model;
+        }else {
+            this.model = "Unknown";
+        }
     }
-
+    //Getter
     public String getModel(){
         return this.model;
     }
