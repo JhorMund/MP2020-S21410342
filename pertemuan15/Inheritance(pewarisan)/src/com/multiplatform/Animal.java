efc36934157f9d0d440d8acfd478fe7dc7949ff0
jhordy <jhordymundung10@gmail.com>
2020-06-26 15:36:00 +0800

    Inheritance

--- pertemuan15/Inheritance(pewarisan)/src/com/multiplatform/Animal.java
+++ pertemuan15/Inheritance(pewarisan)/src/com/multiplatform/Animal.java
@@ -6,5 +6,44 @@ public class Animal {
     private int brain;
     private int height;
 
-}
+    //default constructor
+    public Animal(){
 
+    }
+    public Animal(String name, int body, int brain, int height) {
+        this.name = name;
+        this.body = body;
+        this.brain = brain;
+        this.height = height;
+    }
+
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+
+    public void move(int speed){
+        System.out.println("Animal is moving with speed " + speed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+}
